cc379ff4fffe39c82cd1761ac2dbbd3d0cb30cbc
Veroni99 <mogolivetsveronika@gmail.com>
2025-06-07 23:49:30 +0300

    filter_empty1.py

--- filter_empty.py
+++ filter_empty.py
@@ -1,5 +1,16 @@
-import sys
+def remove_empty_lines(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file: 
+        lines = file.readlines()
 
-for line in sys.stdin:
-    if line.strip():  
-        print(line, end='') 
+    non_empty_lines = [line for line in lines if line.strip() != '']
+
+    with open(file_path, 'w', encoding='utf-8') as file:
+        file.writelines(non_empty_lines)
+
+
+def main():
+    file_path = input()
+    remove_empty_lines(file_path)
+
+if name == 'main':
+    main()
